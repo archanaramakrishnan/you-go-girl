a8e8638e7b18ee875e4eaba960364aa766f19c86
katiehrenchir <katie.hrenchir@gmail.com>
2019-02-09 04:25:17 -0600

    Adds queryset of challenges to challenges page, uggo af tho

--- challenges/models.py
+++ challenges/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 class CommunityChallenge(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField(max_length=200)
+    published_date = models.DateTimeField(blank=True, null=True)
+
     #difficulty level
-    #timestamp
     #set of mentee responses/solutions
     # author = ForeignKey(User)??
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
--- challenges/views.py
+++ challenges/views.py
@@ -4,9 +4,11 @@ from challenges.models import CommunityChallenge
 
 def challenges(request):
     num_challenge_posts = CommunityChallenge.objects.all().count()
+    posts = CommunityChallenge.objects.order_by('-published_date')
 
     context = {
         'num_challenge_posts': num_challenge_posts,
+        'challenges': posts,
     }
 
 
