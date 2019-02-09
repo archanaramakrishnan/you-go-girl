0d7302fb35690ed56d778c586f3232a6c904e07d
katiehrenchir <katie.hrenchir@gmail.com>
2019-02-09 02:28:26 -0600

    Adds you-go-girl.herokuapp.com to allowed hosts

--- yougogirl/settings.py
+++ yougogirl/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'h4rh&z=!9t!+33ul#4zs=7dr%w=b7w+6sy_of(26dps0by!kh0'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com', '127.0.0.1', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['you-go-girl.herokuapp.com', 'yougogirlmentorship.com' '127.0.0.1', '127.0.0.1:8000']
 
 
 # Application definition
