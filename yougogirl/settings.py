b9b1a8eeec454fed40817b04c308f6f780e32fcf
Archana Ramakrishnan <archana2359@gmail.com>
2019-02-09 22:15:25 -0600

    Spanish translations for community challenges page

--- yougogirl/settings.py
+++ yougogirl/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-import dj_database_url 
+import dj_database_url
 import os
 from django.utils.translation import gettext_lazy as _
 
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
