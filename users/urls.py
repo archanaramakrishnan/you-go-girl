e8be4b8f2d6346492337672b564685c26e3a36f5
katiehrenchir <katie.hrenchir@gmail.com>
2019-02-10 07:46:01 -0600

    Adds mentor list page

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # Behavior defined in yougogirl/blog/views.py as blog()
     #URL to access this page: http://127.0.0.1:8000/profile/
     path('profile', views.profile, name='profile'),
+    path('mentor_list', views.mentor_list, name='mentor_list'),
+
     url(r'^$', views.signup, name='signup'),
 
 ]
